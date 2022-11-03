a7fe62133dd9881a4e704f4b78abc2828ca1b4c4
KathCardenas <kath9704.gmail.com>
2022-11-03 11:45:41 -0600

    Controlador prueba 1

--- too/src/Controller/ProfesionController.php
+++ too/src/Controller/ProfesionController.php
@@ -5,14 +5,37 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use App\Entity\Profesion;
 
 class ProfesionController extends AbstractController
 {
+    //Obtener todas las profesiones
     #[Route('/profesion', name: 'app_profesion')]
-    public function index(): Response
+    public function obtenerProfesiones(Request $request)
     {
-        return $this->render('profesion/index.html.twig', [
-            'controller_name' => 'ProfesionController',
-        ]);
+        try{
+            $em = $this->$this->getDoctrine()->getManager();
+
+            $profesiones = em->getRepository(Profesion::class)->findAll();
+
+            $data=['respuesta'=>'OK',
+                    'lista_profesiones'=>'profesiones'];
+
+            return new View($data, Response::HTTP_OK);
+
+
+        }catch(\Exception $e){
+            $view = [
+                "code"=> 500,
+                'content' => 'Ha ocurrido un error',
+                'exception'=> [
+                    'message' => $e->getMessage(),
+                    'code' => $e->getStatusCode(),
+                ]
+                ];
+                return new View($view, Response::HTTP_OK);
+        }
+
     }
 }
